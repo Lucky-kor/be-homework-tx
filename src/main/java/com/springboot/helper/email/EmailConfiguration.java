3b8d3bfd1047fa3f6d6ee6323992a897e834017a
KoKyungBeom <ooo6805@naver.com>
2024-07-02 14:43:23 +0900

    Transaction

--- src/main/java/com/springboot/helper/email/EmailConfiguration.java
+++ src/main/java/com/springboot/helper/email/EmailConfiguration.java
@@ -2,6 +2,7 @@ package com.springboot.helper.email;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @Configuration
 public class EmailConfiguration {
